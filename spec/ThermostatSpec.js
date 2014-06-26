598ff5007b0383e8e1ee46e253956e99333e180c
Julia <juliajtan@gmail.com>
2014-06-26 18:24:13 +0100

    Implemented maximum and minimum temperature, depending on power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,10 +1,32 @@
 describe("Thermostat", function() {
- 
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
   describe("Default settings", function(){
     it('Should be set at 20 degrees', function(){
+      expect(thermostat.temperature).toEqual(20);
+    });
 
-      thermostat = new Thermostat(20);
-      expect(thermostat.temperature).toBe(20);
+    it('PSM should be on', function(){
+      expect(thermostat.powerSavingMode).toBe(true);
+    });
+  });
+
+  describe("Temperature range", function(){
+    it('Minimum temperature of 10 degrees', function(){
+      expect(thermostat.minTemperature()).toEqual(10);
+    });
+
+    it('Maximum temperature of 25 degrees when PSM mode on', function(){
+      expect(thermostat.maxTemperature()).toEqual(25);
+    });
+
+    it('Maximum temperature of 32 degrees when PSM mode off', function(){
+      thermostat.powerSavingMode = false;
+      expect(thermostat.maxTemperature()).toEqual(32);
 
     });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,8 +1,15 @@
-function Thermostat(temperature) {
-  this.temperature = temperature
+function Thermostat() {
+  this.temperature = 20;
+  this.powerSavingMode = true;
 }
 
-// Thermostat.prototype.temperature = function() {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
+Thermostat.prototype.minTemperature = function() {
+  return 10;
+};
+
+Thermostat.prototype.maxTemperature = function() {
+  if (this.powerSavingMode) {
+    return 25;
+  }
+  return 32;
+};
